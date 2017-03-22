005683782afcbbf4325d9c0e742f69197879d6da
Nigel Davila <nayeldavila@gmail.com>
2017-03-22 00:06:05 +0000

    doorkeeper

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+# Use DoorKeeper for user login
+gem 'doorkeeper'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+  
+  root "users#new"
+  
   resources :courses
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: { format: "json" } do 
+    resources :courses, controller: :courses, only: [:index, :show]
+  end 
 end
